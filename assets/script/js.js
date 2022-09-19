dc1e748eeec3f6197460a2a2ecb346defb3a82ab
Nick Prater <NikPratr@gmail.com>
2022-09-19 14:30:15 -0400

    minor changes

--- assets/script/js.js
+++ assets/script/js.js
@@ -1,6 +1,5 @@
 var currentDay = document.querySelector("#currentDay");
 var mainEl = document.querySelector("main");
-var button = document.querySelector(".saveBtn");
 var currentTime = moment().format("k");
 var newContainer = "";
 
@@ -28,24 +27,17 @@ for(i=0;i<24;i++) {
         <div class="row">
         <div class="hour col-md-1">${displayedTime}</div>
         <textarea class="col-md-10${tense}"></textarea>
-        <div class="saveBtn col-md-1"></div>
+        <div class="saveBtn col-md-1" id="${i}"></div>
         </div>
     </div>`
+
+    
+    mainEl.innerHTML = newContainer;
+
+    document.getElementById(i).addEventListener("click", saveContent);
+
 }
 
-//appends rows
-
-mainEl.innerHTML = newContainer;
-
-button.addEventListener("click", saveSheet);
-
-//saves planned tasks to local storage
-
-function saveSheet(event) {
-    //get parent
-    console.log(event.target.parentElement.children(0).textContent);
-    //select 1st child
-    //convert child's text content to number 0-23 via moment
-    //change set the array entry that matches the 0-23 value's text content to match the textarea content
-    //upload the changed array to local storage
+function saveContent() {
+    console.log("hi");
 }
