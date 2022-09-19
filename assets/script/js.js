503dbb1f8eba4697950705b3e10c2b91136cf85b
Nick Prater <NikPratr@gmail.com>
2022-09-19 14:04:36 -0400

    added comments, began working on save function

--- assets/script/js.js
+++ assets/script/js.js
@@ -1,23 +1,51 @@
 var currentDay = document.querySelector("#currentDay");
 var mainEl = document.querySelector("main");
-var currentTime = moment("hA");
+var button = document.querySelector(".saveBtn");
+var currentTime = moment().format("k");
 var newContainer = "";
 
-currentDay.textContent = moment().format("dddd, MMMM Do")
+//sets jumbotron date to current date
+
+currentDay.textContent = moment().format("dddd, MMMM Do");
+
+//creates and formats all of the rows
 
 for(i=0;i<24;i++) {
 
     var displayedTime = moment(i, "k").format("hA");
+
+    if(i<currentTime) {
+        var tense = " past";
+    } else if (i==currentTime) {
+        var tense = " present";
+    } else {
+        var tense = " future";
+    }
 
     newContainer += 
 
     `<div class="container">
         <div class="row">
         <div class="hour col-md-1">${displayedTime}</div>
-        <textarea class="col-md-10" data-number="${i}"></textarea>
+        <textarea class="col-md-10${tense}"></textarea>
         <div class="saveBtn col-md-1"></div>
         </div>
     </div>`
+}
 
-    mainEl.innerHTML = newContainer;
+//appends rows
+
+mainEl.innerHTML = newContainer;
+
+button.addEventListener("click", saveSheet);
+
+//saves planned tasks to local storage
+
+function saveSheet(event) {
+    //get parent
+    console.log(event.target.parentElement.children(0).textContent);
+    //select 1st child
+    //convert child's text content to number 0-23 via moment
+    //change set the array entry that matches the 0-23 value's text content to match the textarea content
+    //upload the changed array to local storage
 }
